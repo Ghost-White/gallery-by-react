173a150a654af7993218ebb1b95ebd3d02cbbb2d
Ghost-White <1964647370@qq.com>
2016-09-06 16:49:31 +0800

    Build the stage of gallery.

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -26,7 +26,7 @@ function getDefaultModules() {
     loaders: [
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}'
       },
       {
         test: /\.sass/,
@@ -34,11 +34,15 @@ function getDefaultModules() {
       },
       {
         test: /\.scss/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}!sass-loader?outputStyle=expanded'
       },
       {
         test: /\.less/,
         loader: 'style-loader!css-loader!less-loader'
+      },
+      {
+        test:/\.json/,
+        loader:'json-loader'
       },
       {
         test: /\.styl/,
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,33 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//let yeomanImage = require('../images/yeoman.png');
+
+//获取图片相关数据
+var imageDatas = require('../data/imageDatas.json');
+
+//利用自执行函数，将图片名信息转换成图片URL信息
+imageDatas = (function genImageURL(iamgeDatasArr){
+	for(var i=0,j=iamgeDatasArr.length;i<j;i++){
+		var singleImageData = iamgeDatasArr[i];
+		singleImageData.imageURL = require('../images/'+singleImageData.fileName); //增添属性imageURL
+		iamgeDatasArr[i] = singleImageData;
+	}
+
+	return iamgeDatasArr;
+})(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-        <div>Hello</div>
-      </div>
+      <section className="stage">
+      	<section className="img-sec">
+      	</section>
+      	<nav className="controller-nav">
+      	</nav>
+      </section>
     );
   }
 }
