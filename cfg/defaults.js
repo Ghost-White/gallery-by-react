884f9b2051f112edce3434ab484a30e28d45a69b
Ghost-White <1964647370@qq.com>
2016-09-06 10:44:04 +0800

    Build the file structure of project by webpack automatically.

---
+++ cfg/defaults.js
@@ -0,0 +1,64 @@
+/**
+ * Function that returns default values.
+ * Used because Object.assign does a shallow instead of a deep copy.
+ * Using [].push will add to the base array, so a require will alter
+ * the base array output.
+ */
+'use strict';
+
+const path = require('path');
+const srcPath = path.join(__dirname, '/../src');
+const dfltPort = 8001;
+
+/**
+ * Get the default modules object for webpack
+ * @return {Object}
+ */
+function getDefaultModules() {
+  return {
+    preLoaders: [     //webpack的loader使得可以在js中通过require引入其他文件或图片
+      {
+        test: /\.(js|jsx)$/,
+        include: srcPath,
+        loader: 'eslint-loader'   //preloaders保证eslint被最先执行
+      }
+    ],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.sass/,
+        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+      },
+      {
+        test: /\.scss/,
+        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
+      },
+      {
+        test: /\.less/,
+        loader: 'style-loader!css-loader!less-loader'
+      },
+      {
+        test: /\.styl/,
+        loader: 'style-loader!css-loader!stylus-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|woff|woff2)$/,
+        loader: 'url-loader?limit=8192'         //当图片大小小于8k时，直接转换为base64，不再引入url
+      },
+      {
+        test: /\.(mp4|ogg|svg)$/,
+        loader: 'file-loader'
+      }
+    ]
+  };
+}
+
+module.exports = {
+  srcPath: srcPath,
+  publicPath: '/assets/',
+  port: dfltPort,
+  getDefaultModules: getDefaultModules
+};
